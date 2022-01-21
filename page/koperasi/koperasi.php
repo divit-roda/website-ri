5cc149a3fe4d1c420c64eb03d4413912dfba6c74
Rian Rahmawan <rheansyafiq>
2022-01-21 20:56:35 +0700

    updated koperasi2

--- page/koperasi/koperasi.php
+++ page/koperasi/koperasi.php
@@ -30,3 +30,45 @@
     </svg>
 </section>
 <!-- Akhir Jumbotron -->
+
+<section class="privacy section-padding">
+    <div class="container">
+        <div class="service-features desc">
+            <div class="service-feature-item">
+                <div class="section-title">
+                    <h3>“Dari…Oleh…dan untuk MITRA RI”</h3>
+                </div>
+                <h5>Koperasi RI </h5>
+                <ol>
+                    <p>Adalah Koperasi Konsumen yang didirikan sebagai usaha bersama dan merupaka bagian dari RODA INDONESIA.
+Koperasi RI mewadahi kegiatan dan partisipasi para driver, produsen serta mitra RODA INDONESIA dalam membangun kebersamaan dan kesejahteraan bersama.
+
+Anggota Koperasi serta merta menjadi bagian dari RODA INDONESIA, dimana akan ada banyak keuntungan- keuntungan atau manfaat yang diterima, termasuk adanya perlindungan investasi kerja untuk masa depan para anggotanya. Koperasi RI adalah sebuah perencanaan dalam rangka ikut memikirkan masa depan dan kelangsungan kerja serta kesejahteraan semua anggota koperasi RI.</p>
+            </div>
+            <div class="service-feature-item">
+                <h5>KEUNTUNGAN TAHUNAN KOPERASI RI</h5>
+                <p>Partisipasi Aktif anggota Koperasi akan di perhitungkan dalam SHU (Sisa Hasil Usaha) Koperasi RI, yg akan di bagikan setiap satu tahun sekali, yang dapat berupa :</p>
+                <ol>
+                    <li>Jasa Modal </li>
+                    <li>Jasa Perdagangan</li>
+                    <li>Jasa Simpan Pinjam</li>
+                </ol>
+                <h5>KEUNTUNGAN LAIN</h5>
+                <p>Partisipasi Aktif anggota Koperasi akan di perhitungkan dalam SHU (Sisa Hasil Usaha) Koperasi RI, yg akan di bagikan setiap satu tahun sekali, yang dapat berupa :</p>
+                <ol>
+                    <li>Jasa Simpanan Sukarela </li>
+                    <li>Jasa Investasi </li>
+                    <li>THR/Bingkisan Lebaran</li>
+                </ol>
+            </div>
+            <div class="service-feature-item">
+            <h5>HASIL KERJA DAN TABUNGAN SEBAGAI JENJANG KARIER & INVESTASI MASA DEPAN</h5>
+                <p>Anggota Koperasi RI dapat memperkuat Koperasi dengan memperbanyak tabungan sukarela yang sekaligus dapat di konversi menjadi suatu investasi untuk masa depan anggota itu sendiri
+
+Koperasi akan menyisihkan sebagian dari keuntungannya untuk Edukasi Anggota, yang dapat di manfaatkan anggota untuk menimba ilmu sekaligus meningkatkan skill yang dapat menunjang karier dan peningkatan karir, yang bersinergi dengan Roda Indonesia melalui RIEDU.</p>
+               
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
